cc40c37d44489d9821bfccec86ac518f7e6f725c
Utsho <pipul.boddyo@gmail.com>
2022-03-11 14:51:23 +0600

    description works correctly

--- js/app.js
+++ js/app.js
@@ -27,8 +27,8 @@ const reportPost = (id) => {
 };
 
 const displayContent = (text) => {
-  return text.length < 30 ? 'text' : text.slice(0, 30) + "<span class='fw-bold'>... read more</span>";
-};
+  return text.length < 30 ? text : text.slice(0, 30) + "<span class='fw-bold'>... read more</span>";
+};  /* if description length more then 30, it will show 'read more' else full text */
 
 const switchTab = (id) => {
   if (id === "posts") {
@@ -52,6 +52,7 @@ const switchTab = (id) => {
 
 const createPost = (post) => {
   const image = post.image;
+  const userImage = post.userImage;   /* User Image added to the top of the every post */
   const div = document.createElement("article");
   div.classList.add("post");
   div.innerHTML = `
@@ -61,7 +62,7 @@ const createPost = (post) => {
                     href="https://github.com/ProgrammingHero1"
                     target="_blank"
                     class="post__avatar">
-                    <img src="${image}" alt="User Picture" />
+                    <img src="${userImage}" alt="User Picture" />
                   </a>
                   <a href="#" class="post__user">phero</a>
                 </div>
@@ -85,8 +86,8 @@ const createPost = (post) => {
                 <div class="post__buttons">
                   <button class="post__button" onclick="addToLiked(${post.id})">
                   <i class="fa-solid fa-heart ${isLiked(post.id) && "text-danger"}"></i>
-                    
                   </button>
+
                   <button class="post__button">
                     <i class="fa-solid fa-comment"></i>
                   </button>
@@ -100,12 +101,12 @@ const createPost = (post) => {
                   </button>
                 </div>
 
-                <div class="post__content">${displayContent(post.description)}</div>
+                <div class="post__content">${displayContent(post.description)  /* Description goes here */}</div>
 
                 <div class="post__infos">
                   <div class="post__likes">
                     <a href="#" class="post__likes-avatar">
-                      <img src="https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80" alt="User Picture" />
+                      <img src="${userImage}" alt="User Picture" />
                     </a>
 
                     <span>Liked by
@@ -118,9 +119,9 @@ const createPost = (post) => {
                   <div class="post__description">
                     <small>
                       <a class="post__name--underline" href="#">
-                          ${post.comments?.user}
+                          ${post.comments[0]?.user /* Commented username added */}
                       </a>
-                      ${post.comments?.text}
+                      ${post.comments[0]?.text  /* Comment goes here */}
                     </small>
                   </div>
                   <span class="post__date-time">30 minutes ago</span>
@@ -159,6 +160,7 @@ const displayReportedPosts = () => {
 const loadPosts = async () => {
   let data = await fetch('../data/posts.json');
   posts = await data.json();
+  console.log(posts);
   showPosts(posts);
 }
 
